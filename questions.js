b0c02f4713991e1f1806943edabefbadbae7e10d
QUARK88 <72112478+QUARK88@users.noreply.github.com>
2021-03-13 08:07:53 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,15 @@
+questions = [
+    {
+        "question": "",
+        "effect": {
+            "bregu": 10,
+            "btech": 10,
+            "binte": 10,
+            "bince": 10,
+            "bcent": 10,
+            "bstru": 10,
+            "bland": 10,
+            "btrad": 10,
+        }
+    }
+];
