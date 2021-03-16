584b30c0c477fa9cf121d8d1a48bb360398fd65c
QUARK88 <72112478+QUARK88@users.noreply.github.com>
2021-03-16 08:18:16 -0400

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -11,5 +11,57 @@ ideologies = [
             "bland": 50,
             "btrad": 50,
         }
+    },
+    {
+        "name": "Quarkism",
+        "stats": {
+            "bregu": 60,
+            "btech": 75,
+            "binte": 20,
+            "bince": 25,
+            "bcent": 70,
+            "bstru": 60,
+            "bland": 40,
+            "btrad": 90,
+        }
+    },
+    {
+        "name": "FALGSC",
+        "stats": {
+            "bregu": 100,
+            "btech": 100,
+            "binte": 100,
+            "bince": 100,
+            "bcent": 100,
+            "bstru": 100,
+            "bland": 100,
+            "btrad": 0,
+        }
+    },
+    {
+        "name": "Capacitationism",
+        "stats": {
+            "bregu": 75,
+            "btech": 100,
+            "binte": 100,
+            "bince": 50,
+            "bcent": 40,
+            "bstru": 70,
+            "bland": 100,
+            "btrad": 0,
+        }
+    },
+    {
+        "name": "Nyuffdasha",
+        "stats": {
+            "bregu": 10,
+            "btech": 50,
+            "binte": 90,
+            "bince": 0,
+            "bcent": 10,
+            "bstru": 25,
+            "bland": 0,
+            "btrad": 10,
+        }
     }
 ];
