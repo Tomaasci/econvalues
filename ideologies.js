d34dbd42f51acce38ea3f16302e2a2a20b0e93dd
Quark <72112478+QUARK88@users.noreply.github.com>
2021-04-10 18:13:36 -0400

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -2,105 +2,14 @@ ideologies = [
     {
         "name": "Centrism",
         "stats": {
-            "bregu": 50,
-            "btech": 50,
-            "binte": 50,
             "bince": 50,
-            "bcent": 50,
             "bstru": 50,
-            "bland": 50,
-            "btrad": 50,
-        }
-    },
-    {
-        "name": "Luxury Communism",
-        "stats": {
-            "bregu": 90,
-            "btech": 100,
-            "binte": 70,
-            "bince": 100,
-            "bcent": 70,
-            "bstru": 65,
-            "bland": 100,
-            "btrad": 5,
-        }
-    },
-    {
-        "name": "Marxism Leninism",
-        "stats": {
-            "bregu": 80,
-            "btech": 60,
-            "binte": 10,
-            "bince": 70,
-            "bcent": 90,
-            "bstru": 60,
-            "bland": 80,
-            "btrad": 50,
-        }
-    },
-    {
-        "name": "Council Communism",
-        "stats": {
-            "bregu": 90,
-            "btech": 70,
-            "binte": 25,
-            "bince": 80,
-            "bcent": 45,
-            "bstru": 90,
-            "bland": 90,
-            "btrad": 45,
-        }
-    },
-    {
-        "name": "Mutualism",
-        "stats": {
-            "bregu": 40,
-            "btech": 60,
-            "binte": 90,
-            "bince": 30,
-            "bcent": 0,
-            "bstru": 100,
-            "bland": 100,
-            "btrad": 50,
-        }
-    },
-    {
-        "name": "Conservatism",
-        "stats": {
-            "bregu": 50,
-            "btech": 25,
-            "binte": 20,
-            "bince": 20,
-            "bcent": 0,
-            "bstru": 50,
-            "bland": 95,
-            "btrad": 70,
-        }
-    },
-    {
-        "name": "Neoliberalism",
-        "stats": {
-            "bregu": 65,
-            "btech": 75,
-            "binte": 95,
-            "bince": 15,
+            "binte": 50,
             "bcent": 50,
-            "bstru": 20,
-            "bland": 25,
-            "btrad": 0,
-        }
-    },
-    {
-        "name": "Social Democracy",
-        "stats": {
-            "bregu": 90,
             "btech": 50,
-            "binte": 60,
-            "bince": 90,
-            "bcent": 50,
-            "bstru": 45,
             "bland": 50,
-            "btrad": 50,
+            "binhe": 50,
+            "blabo": 50,
         }
     }
 ];
