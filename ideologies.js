de3383a42c1db9ad1232cfb702ef59d12b7ef2c2
Quark <72112478+QUARK88@users.noreply.github.com>
2021-04-15 15:33:07 -0400

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -11,5 +11,18 @@ ideologies = [
             "binhe": 50,
             "blabo": 50,
         }
+    },
+    {
+        "name": "Anarcho-Communism",
+        "stats": {
+            "bince": 100,
+            "bstru": 100,
+            "binte": 30,
+            "bcent": 00,
+            "btech": 50,
+            "bland": 100,
+            "binhe": 10,
+            "blabo": 75,
+        }
     }
 ];
