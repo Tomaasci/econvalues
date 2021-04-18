4ce467e3e5ab18006347c2c36b8966b03a1f65f2
Quark <72112478+QUARK88@users.noreply.github.com>
2021-04-18 17:38:27 -0400

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -76,5 +76,148 @@ ideologies = [
             "binhe": 30,
             "blabo": 40,
         }
+    },
+    {
+        "name": "Neoliberalism ",
+        "stats": {
+            "bince": 25,
+            "bstru": 25,
+            "binte": 90,
+            "bcent": 60,
+            "btech": 80,
+            "bland": 40,
+            "binhe": 10,
+            "blabo": 20,
+        }
+    },
+    {
+        "name": "Dirigisme",
+        "stats": {
+            "bince": 50,
+            "bstru": 0,
+            "binte": 30,
+            "bcent": 100,
+            "btech": 60,
+            "bland": 40,
+            "binhe": 25,
+            "blabo": 40,
+        }
+    },
+    {
+        "name": "Distributism",
+        "stats": {
+            "bince": 60,
+            "bstru": 70,
+            "binte": 50,
+            "bcent": 0,
+            "btech": 25,
+            "bland": 50,
+            "binhe": 50,
+            "blabo": 50,
+        }
+    },
+    {
+        "name": "Georgism",
+        "stats": {
+            "bince": 40,
+            "bstru": 50,
+            "binte": 90,
+            "bcent": 40,
+            "btech": 70,
+            "bland": 100,
+            "binhe": 20,
+            "blabo": 50,
+        }
+    },
+    {
+        "name": "Social Democracy",
+        "stats": {
+            "bince": 90,
+            "bstru": 55,
+            "binte": 50,
+            "bcent": 60,
+            "btech": 45,
+            "bland": 60,
+            "binhe": 10,
+            "blabo": 70,
+        }
+    },
+    {
+        "name": "Guild Socialism",
+        "stats": {
+            "bince": 70,
+            "bstru": 70,
+            "binte": 50,
+            "bcent": 100,
+            "btech": 0,
+            "bland": 80,
+            "binhe": 40,
+            "blabo": 70,
+        }
+    },
+    {
+        "name": "Anarcho-Syndicalism",
+        "stats": {
+            "bince": 100,
+            "bstru": 100,
+            "binte": 40,
+            "bcent": 30,
+            "btech": 45,
+            "bland": 100,
+            "binhe": 10,
+            "blabo": 100,
+        }
+    },
+    {
+        "name": "Syndicalism",
+        "stats": {
+            "bince": 80,
+            "bstru": 75,
+            "binte": 30,
+            "bcent": 60,
+            "btech": 45,
+            "bland": 100,
+            "binhe": 10,
+            "blabo": 100,
+        }
+    },
+    {
+        "name": "Yellow Socialism",
+        "stats": {
+            "bince": 70,
+            "bstru": 50,
+            "binte": 20,
+            "bcent": 100,
+            "btech": 20,
+            "bland": 50,
+            "binhe": 40,
+            "blabo": 100,
+        }
+    },
+    {
+        "name": "Palace Economy",
+        "stats": {
+            "bince": 40,
+            "bstru": 0,
+            "binte": 10,
+            "bcent": 100,
+            "btech": 30,
+            "bland": 60,
+            "binhe": 100,
+            "blabo": 20,
+        }
+    },
+    {
+        "name": "Feudalism",
+        "stats": {
+            "bince": 15,
+            "bstru": 0,
+            "binte": 40,
+            "bcent": 0,
+            "btech": 10,
+            "bland": 0,
+            "binhe": 100,
+            "blabo": 20,
+        }
     }
 ];
